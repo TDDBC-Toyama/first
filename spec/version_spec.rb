63f2f4aa8e800fae110618c18655c9d1e267bf71
suginoy <suginoyasuhiro@gmail.com>
2017-02-18 15:57:18 +0900

    patch まで終わり

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,12 +1,17 @@
 require_relative '../version.rb'
 describe Version do
-  describe 'major 番号を与えて、major 番号を返す' do
+  describe 'major 番号を与えて、 major 番号を返す' do
     it { expect(Version.new(major: 1).major).to eq '1' }
     it { expect(Version.new(major: 2).major).to eq '2' }
   end
 
-  describe 'minor 番号を与えて、minor 番号を返す' do
+  describe 'minor 番号を与えて、 minor 番号を返す' do
     it { expect(Version.new(minor: 1).minor).to eq '1' }
     it { expect(Version.new(minor: 2).minor).to eq '2' }
+  end
+
+  describe 'patch 番号を与えて、 patch 番号を返す' do
+    it { expect(Version.new(patch: 1).patch).to eq '1' }
+    it { expect(Version.new(patch: 2).patch).to eq '2' }
   end
 end
--- version.rb
+++ version.rb
@@ -1,7 +1,7 @@
 class Version
-  attr_accessor :major, :minor
+  attr_accessor :major, :minor, :patch
 
-  def initialize(major: nil, minor: nil)
-    @major, @minor = major.to_s, minor.to_s
+  def initialize(major: nil, minor: nil, patch: nil)
+    @major, @minor, @patch = major.to_s, minor.to_s, patch.to_s
   end
 end
