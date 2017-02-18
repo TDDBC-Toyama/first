ce815f977e68a28c952498caf9958250c1df6bc6
suginoy <suginoyasuhiro@gmail.com>
2017-02-18 17:16:35 +0900

    attr_accessor -> attr_reader

--- version.rb
+++ version.rb
@@ -1,5 +1,5 @@
 class Version
-  attr_accessor :major, :minor, :patch
+  attr_reader :major, :minor, :patch
 
   def initialize(major, minor = 0, patch = 0)
     @major, @minor, @patch = major.to_s, minor.to_s, patch.to_s
