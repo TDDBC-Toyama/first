1d0029915107eec32fbd4b6b81174af5b1e2c2a9
suginoy <suginoyasuhiro@gmail.com>
2017-02-19 09:56:45 +0900

    wip

--- version.rb
+++ version.rb
@@ -1,9 +1,18 @@
 class Version
   class VersionValidationError < StandardError; end
   def initialize(major, minor = 0, patch = 0)
-    raise VersionValidationError, 'major にマイナスは指定できません' if major < 0
-    raise VersionValidationError, 'minor にマイナスは指定できません' if minor < 0
+    if (major < 0 || minor < 0)
+      raise VersionValidationError, 'major にマイナスは指定できません' if major < 0
+      raise VersionValidationError, 'major にマイナスは指定できません' if major < 0
+      raise VersionValidationError, 'minor にマイナスは指定できません' if minor < 0
+    end
     @major, @minor, @patch = major, minor, patch
+  end
+
+  def check_validation(num)
+    if (num < 0)
+      raise VersionValidationError, "#{num} にマイナスは指定できません"
+    end
   end
 
   [:major, :minor, :patch].each do |method|
